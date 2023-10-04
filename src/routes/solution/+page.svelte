f42e18e1a7a4de2620f77260e1f6847f5c4e95b6
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2023-10-04 08:51:07 -0300

    Update +page.svelte
    Loading component implemented.

--- src/routes/solution/+page.svelte
+++ src/routes/solution/+page.svelte
@@ -1,8 +1,13 @@
 <script>
     /** @type {import('./$types').PageData} */
+
+    import Loading from "$lib/components/loading.svelte";
+
     // @ts-ignore
     export let data;
 </script>
 
 <h1>Solution page. This is the solution:</h1>
 <h1>{data.id}</h1>
+
+<Loading />
