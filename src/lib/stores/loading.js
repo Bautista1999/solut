9fb7fdfc98ff77fbe0a94092e8eb634927dcbbee
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2023-10-23 21:42:10 -0300

    Update loading.js

--- src/lib/stores/loading.js
+++ src/lib/stores/loading.js
@@ -10,4 +10,11 @@ export const termsModal = writable(false);
 export const subideaModal = writable(false);
 export const subideaLoading = writable(false);
 export const PostUpdateModal = writable(false);
+/**
+ * @type {string[]}
+ */
+let images = [];
+export const updateImages = writable(images);
+export const displayedUpdImages = writable(images);
+
 
