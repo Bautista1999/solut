fc79e3da8ea4e3242684ca565a65a68993c2043c
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2023-10-28 19:37:04 -0300

    Update +page.svelte

--- src/routes/homepage/+page.svelte
+++ src/routes/homepage/+page.svelte
@@ -1,15 +1,15 @@
 <script>
-    // @ts-nocheck
-
     import Header from "$lib/components/header.svelte";
     import { TabGroup, Tab, TabAnchor } from "@skeletonlabs/skeleton";
+    import Layout from "../+layout.svelte";
     import { initJuno, listDocs } from "@junobuild/core";
     import { onMount } from "svelte";
     import { get } from "svelte/store";
     import { Modal, modalStore } from "@skeletonlabs/skeleton";
     import { goto } from "$app/navigation";
-
-    // @ts-nocheck
+    import { listIdeas } from "$lib/data_functions/docu.functions";
+    import TopicBadge from "$lib/components/topicBadge.svelte";
+    import { createJunoTopic } from "$lib/data_objects/data_objects";
 
     // @ts-ignore
     /**
@@ -20,11 +20,36 @@
      * @type {any[]}
      */
     let owners = [];
+    let percentage = 80;
     let n = 10; // suppose the length of your array is 10
     let indices = Array.from({ length: n }, (_, i) => i);
     let showModal = false;
+    let topic = createJunoTopic();
+    /**
+     * @type {topic []}
+     */
+    let mostFunded = [];
+    /**
+     * @type {topic []}
+     */
+    let techIdeas = [];
+    /**
+     * @type {topic []}
+     */
+    let recentIdeas = [];
+    /**
+     * @type {topic []}
+     */
+    let popularIdeas = [];
     let imageSrc = "/assets/appImageExample.jpeg";
     let id = "33";
+    /**
+     * @param {any} title
+     * @param {any} subtitle
+     * @param {any} description
+     * @param {string} image
+     * @param {string} key
+     */
     function showIdea(title, subtitle, description, image, key) {
         showModal = true;
         console.log(key);
@@ -37,48 +62,43 @@
         body: "This is an example modal.",
         image: "/assets/appImageExample.jpeg",
     };
-    let titulo = "";
     let topicsLoading = true;
 
     let showSearch = false;
     // function limitDescription(string: desc) {}
     onMount(async () => {
-        await initJuno({
-            satelliteId: "vehbc-zaaaa-aaaal-acyba-cai",
-        });
-        console.log("Mounted");
         const MyList = await listDocs({
             collection: "ideas",
         });
-        console.log(MyList);
 
-        let ideas = MyList.items;
-        for (let i = 0; i < ideas.length; i++) {
-            const idea = {
-                title: "",
-                subtitle: "",
-                description: "",
-                image: "",
-                key: "",
-            };
-            idea.title = ideas[i].data.title;
-            owners.push(ideas[i].owner);
-            idea.subtitle = ideas[i].data.subtitle;
-            idea.description = ideas[i].data.description;
-            idea.image = ideas[i].data.image;
-            idea.key = ideas[i].key;
-            list.push(idea);
-        }
         // @ts-ignore
-        console.log(list);
+        mostFunded = await listIdeas("none", "funding", "ideas", "all", false);
+        // @ts-ignore
+        techIdeas = await listIdeas(
+            "🦾 Technology",
+            "none",
+            "ideas",
+            "all",
+            false
+        );
+        // @ts-ignore
+        recentIdeas = await listIdeas("none", "recent", "ideas", "all", false);
+        // @ts-ignore
+        popularIdeas = await listIdeas(
+            "none",
+            "popularity",
+            "ideas",
+            "all",
+            false
+        );
+        console.log("Ordered ideas", mostFunded);
+        // @ts-ignore
         n = list.length;
-        console.log(n);
         indices = Array.from({ length: n }, (_, i) => i);
         topicsLoading = false;
     });
 </script>
 
-<Header />
 <div class="div">
     <h1 class="h1" style="font-family: Barlow;">
         <span
@@ -96,121 +116,242 @@
     </h1>
 
     <br />
-    <h1 class="title" style="font-family: Barlow;">Welcome to Solutio.</h1>
-</div>
-{#if showSearch}
-    <input class="input" type="search" name="demo" placeholder="Search..." />
-{/if}
-<TabGroup class="text-2xl px-10 py-5" />
-
-<div class="snap-x">
-    <h2 class="h3 trending" style="font-family: Barlow;">Trending</h2>
+    <h1 class="title" style="font-family: Barlow; line-height:1.1;">
+        Welcome to Solutio.
+    </h1>
 </div>
 <div
-    class="snap-x scroll-px-4 snap-mandatory scroll-smooth flex gap-4 overflow-x-auto px-4 py-10"
->
-    {#each indices as index (index)}
-        {#if topicsLoading}
-            <div class="snap-start card w-50 h-80 md:w-80">
-                <div
-                    class=" py-20 w-80 placeholder animate-pulse rounded-top"
-                />
-                <div
-                    class="snap-start py-3 h-3 w-20 md:w-40 placeholder animate-pulse"
-                    style="margin-left: 5%; margin-top: 5%"
-                />
-                <div
-                    class="snap-start py-2 h-2 md:w-70 placeholder animate-pulse"
-                    style="margin-left: 5%;margin-right: 5%; margin-top: 5%"
-                />
-                <div
-                    class="snap-start py-2 h-2 md:w-70 placeholder animate-pulse"
-                    style="margin-left: 5%;margin-right: 5%; margin-top: 5%"
-                />
-                <div
-                    class="snap-start py-2 h-2 md:w-20 placeholder animate-pulse"
-                    style="margin-left: 5%;margin-right: 5%; margin-top: 5%"
+    class="horizontalLine"
+    style="border-color: aliceblue; width:80%; border-width:0.5px; margin-left:auto;margin-right:auto; opacity:0.5;"
+/>
+<div class="itemsDisplay">
+    <h1 class="elementTitle">Most funded topics</h1>
+    <div class="projectsSection">
+        {#each mostFunded as topic}
+            <div style="width: fit-content;">
+                <TopicBadge
+                    topic={topic.data}
+                    creator={topic.owner}
+                    key={topic.key}
                 />
             </div>
-        {:else}
-            <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <div
-                class="card-container"
-                style="height: 11.5cm;  "
-                role="button"
-                tabindex="0"
-                on:click={() =>
-                    showIdea(
-                        list[index]?.title,
-                        list[index]?.subtitle,
-                        list[index]?.description,
-                        imageSrc,
-                        list[index]?.key
-                    )}
-                on:keydown={() =>
-                    showIdea(
-                        list[index]?.title,
-                        list[index]?.subtitle,
-                        list[index]?.description,
-                        list[index]?.key,
-                        imageSrc
-                    )}
-            >
-                <div
-                    class="snap-start card-hover card md:w-80 text-start"
-                    style="height: 11.5cm; position: relative; "
-                >
-                    <div
-                        class=" py-16 w-80 text-center rounded-top image-container"
-                        style=" background-image: url('/assets/appImageExample.jpeg');"
-                    />
-
-                    <p class="h3" style="margin-left: 5%;margin-top: 5%;">
-                        {list[index]?.title}
-                    </p>
-                    <TabGroup class="text-2xl px-5 py-2" />
-
-                    <p class="h5" style="margin-left: 5%;margin-right: 5%;">
-                        {list[index]?.subtitle}
-                    </p>
-
-                    {#if list[index]?.description.length > 145}
-                        <p style="margin-left: 5%; margin-right: 5%">
-                            {list[index]?.description.substring(0, 145)}...<a
-                                href="/homepage"
-                                role="button"
-                                on:click={showIdea}
-                                on:keydown={showIdea}
-                                on:keyup={showIdea}
-                                style="text-decoration: underline; cursor: pointer; "
-                                >Read more</a
-                            >
-                        </p>
-                    {:else}
-                        <p style="margin-left: 5%; margin-right: 5%; ">
-                            {list[index]?.description}
-                        </p>
-                    {/if}
-                    <div
-                        class="rounded-bottom w-80"
-                        style="position: absolute; bottom: 0; background-color:#EC5800;"
-                    >
-                        <p
-                            style="margin-left: 5%; margin-right: 5%; margin-bottom:4%; margin-top: 4%;"
-                        >
-                            Creator: {owners[index]}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        {/if}
-    {/each}
+        {/each}
+    </div>
 </div>
+<div class="itemsDisplay">
+    <h1 class="elementTitle">Popular project solutions</h1>
+    <div class="projectsSection">
+        {#each popularIdeas as topic}
+            <div style="width: fit-content;">
+                <TopicBadge
+                    topic={topic.data}
+                    creator={topic.owner}
+                    key={topic.key}
+                />
+            </div>
+        {/each}
+    </div>
+</div>
+<div class="itemsDisplay">
+    <h1 class="elementTitle">Technology</h1>
+    <div class="projectsSection">
+        {#each techIdeas as topic}
+            <div style="width: fit-content;">
+                <TopicBadge
+                    topic={topic.data}
+                    creator={topic.owner}
+                    key={topic.key}
+                />
+            </div>
+        {/each}
+    </div>
+</div>
+<div class="itemsDisplay">
+    <h1 class="elementTitle">Most recent</h1>
+    <div class="projectsSection">
+        {#each recentIdeas as topic}
+            <div style="width: fit-content;">
+                <TopicBadge
+                    topic={topic.data}
+                    creator={topic.owner}
+                    key={topic.key}
+                />
+            </div>
+        {/each}
+    </div>
+</div>
+
 {#if showModal}
     <Modal />
 {/if}
 
 <style>
+    .itemsDisplay {
+        margin-left: 10%;
+        margin-top: 20px;
+    }
+    .elementTitle {
+        font-size: 30px;
+        opacity: 0.8;
+        font-weight: 400;
+        line-height: 1.5;
+        color: white;
+    }
+    .projectsSection {
+        height: fit-content;
+        width: 150vh;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        /* border-color: black;
+        border-width: 2px;
+        background-color: rgba(255, 128, 0, 0.863); */
+        background-size: cover; /* Optional: Scale the image to cover the entire element */
+        background-position: center; /* Optional: Position the image in the center */
+        background-repeat: no-repeat;
+        max-width: 150vh;
+        padding-left: 20px;
+        gap: 40px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        overflow-x: auto;
+    }
+    .verticalLine {
+        height: 1cm;
+        border-color: #ff6000;
+        border-width: 0.5px;
+    }
+    .horizontalLine {
+        width: 100%;
+        border-color: black;
+        border-width: 0.5px;
+    }
+    .followersInfo {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 40px;
+    }
+    .centeredImage {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .creatorPicture {
+        width: 0.8cm;
+        height: 0.8cm;
+        overflow: hidden;
+        border: 1px solid black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .creatorPicture img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    .barra {
+        width: 90%;
+        height: 30px;
+        margin-top: 0px;
+        margin-bottom: 0px;
+        margin-left: auto;
+        margin-right: auto;
+        display: flex;
+        align-items: center;
+        background-color: #e0e0e0;
+        border-radius: 2px;
+        overflow: visible;
+        border-style: groove;
+        border-width: 1px;
+        border-color: black;
+    }
+    .progreso {
+        height: 100%;
+        background: linear-gradient(
+            to right,
+            orange,
+            orangered
+        ); /* Naranja claro a naranja fuerte */
+        transition: width 0.3s;
+        padding: 1%;
+        display: flex;
+        align-items: center; /* Vertical alignment */
+        justify-content: center; /* Horizontal alignment */
+        color: #e0e0e0;
+        font-size: larger;
+        font-weight: 450;
+        border-radius: 2px;
+    }
+    .progreso2 {
+        margin: 3%;
+        flex: 1; /* Ocupará todo el espacio restante después del div progreso */
+        display: flex;
+        align-items: center; /* Alineación vertical en el centro */
+        justify-content: flex-start; /* Alineación horizontal al inicio */
+        color: orangered;
+        font-size: larger;
+    }
+    .projectBody {
+        padding-top: 15px;
+        padding-left: 15px;
+        padding-right: 15px;
+        padding-bottom: 5px;
+        line-height: 1.5;
+    }
+    .profilePicture {
+        width: 100%;
+        height: 40%;
+        overflow: hidden;
+        border: 1px solid black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .profilePicture img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .infoSection {
+        width: 100%;
+        height: 12.7cm;
+    }
+    .footerSection {
+        width: 100%;
+        height: 1.3cm;
+        padding-left: 5px;
+        padding-top: 6px;
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+        color: antiquewhite;
+        background: linear-gradient(
+            to right,
+            #ff6000,
+            rgb(255, 132, 9),
+            #ff6000
+        );
+    }
+    .project {
+        width: 10cm;
+        height: 14cm;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        text-align: left;
+        background-color: antiquewhite;
+        color: darkslategray;
+        border-color: black;
+        border-width: 2px;
+        box-shadow: 10px 10px 0px rgb(0, 0, 0, 0.5);
+    }
     .image-container {
         background-size: cover; /* Scale the image to cover the entire element */
         background-position: center; /* Center the image in the container */
