fee0866bfc837fd7c9b173723b95410ce682d125
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2023-11-01 12:22:57 -0300

    Create loadingBadge.svelte

---
+++ src/lib/components/loadingBadge.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { onMount } from "svelte";
+    import MagicalDots from "./magicalDots.svelte";
+
+    export let msg = "Data loading";
+    onMount(async () => {});
+</script>
+
+<div class="project">
+    <MagicalDots />
+    <br />
+    <p>{msg}</p>
+</div>
+
+<style>
+    .project {
+        width: 10cm;
+        height: 14cm;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: antiquewhite;
+        color: darkslategray;
+        border-color: black;
+        border-width: 2px;
+        box-shadow: 10px 10px 0px rgb(0, 0, 0, 0.5);
+    }
+    @media (max-width: 768px) {
+        .project {
+            width: 8cm;
+            height: 14cm;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-color: antiquewhite;
+            color: darkslategray;
+            border-color: black;
+            border-width: 2px;
+            box-shadow: 10px 10px 0px rgb(0, 0, 0, 0.5);
+        }
+    }
+</style>
