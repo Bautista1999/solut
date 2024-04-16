663b97d400cb1f33c66d49c8f58eefeb3422aad2
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2024-04-16 16:56:55 -0300

    Create usersOverview.svelte

---
+++ src/lib/components/usersOverview.svelte
@@ -0,0 +1,124 @@
+<script>
+    // Import the images or define their URLs
+    /**
+     * @type {string[]}
+     */
+    export let images = [];
+    export let card = false;
+</script>
+
+{#if card}
+    <div class="image-scroller_card" style="">
+        {#each images as image, index (image)}
+            <div
+                class="image-container_card"
+                style="left: {index * 15}px; z-index: {100 - index}; "
+            >
+                <img src={image} alt={`Image number ${index + 1}`} />
+            </div>
+        {/each}
+    </div>
+{:else}
+    <div class="image-scroller">
+        {#each images as image, index (image)}
+            <div
+                class="image-container"
+                style="left: {index * 25}px; z-index: {100 - index};"
+            >
+                <img src={image} alt={`Image number ${index + 1}`} />
+            </div>
+        {/each}
+    </div>
+{/if}
+
+<style>
+    .image-scroller {
+        margin-top: 15px;
+        position: absolute;
+        height: 0px; /* Adjust if necessary */
+        display: flex;
+        /* Removed justify-content: center; to allow starting from the beginning */
+        flex-wrap: nowrap; /* Prevent images from wrapping onto multiple lines */
+    }
+
+    .image-container {
+        /* Removed position: absolute; to keep images in the flow of the document */
+        margin-right: -25px; /* Adjust space between images */
+        border-radius: 50%; /* Make images round */
+        overflow: hidden; /* Hide overflow to maintain the circular shape */
+        width: 45px; /* Width of the images */
+        height: 45px; /* Height of the images */
+        border: 2px solid var(--secondary-color);
+        cursor: pointer;
+        transition:
+            border-color 0.3s ease,
+            width 0.1s ease,
+            height 0.1s ease;
+    }
+
+    .image-container:last-child {
+        margin-right: 3px; /* No margin for the last image */
+    }
+    .image-container:hover {
+        bottom: 0; /* Align images to the bottom */
+        border-radius: 50%; /* Make images round */
+        overflow: hidden; /* Hide overflow to maintain the circular shape */
+        width: 60px; /* Width of the images */
+        height: 60px; /* Height of the images */
+        border: 2px solid var(--primary-color);
+        z-index: 300;
+    }
+    .image-container:active {
+        bottom: 0; /* Align images to the bottom */
+        border-radius: 50%; /* Make images round */
+        overflow: hidden; /* Hide overflow to maintain the circular shape */
+        width: 45px; /* Width of the images */
+        height: 45px; /* Height of the images */
+        border: 2px solid var(--primary-color);
+    }
+
+    .image-container img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover; /* Ensure images cover the container without distortion */
+    }
+    .image-scroller_card {
+        position: absolute;
+        height: 0px; /* Adjust if necessary */
+        display: flex;
+        /* Removed justify-content: center; to allow starting from the beginning */
+        flex-wrap: nowrap; /* Prevent images from wrapping onto multiple lines */
+        margin-top: 0px;
+        height: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .image-container_card {
+        /* Removed position: absolute; to keep images in the flow of the document */
+        margin-right: -25px; /* Adjust space between images */
+        border-radius: 50%; /* Make images round */
+        overflow: hidden; /* Hide overflow to maintain the circular shape */
+        width: 45px; /* Width of the images */
+        height: 45px; /* Height of the images */
+        border: 2px solid var(--secondary-color);
+        width: 30px;
+        position: absolute;
+        height: 30px;
+        transition:
+            border-color 0.3s ease,
+            width 0.1s ease,
+            height 0.1s ease;
+    }
+
+    .image-container_card:last-child {
+        margin-right: 3px; /* No margin for the last image */
+    }
+
+    .image-container_card img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover; /* Ensure images cover the container without distortion */
+    }
+</style>
