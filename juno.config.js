dee664eadfb04296117751dd281f315aaebba465
David Dal Busco <david.dalbusco@outlook.com>
2024-04-29 17:39:56 +0200

    build: juno config as js

---
+++ juno.config.js
@@ -0,0 +1,9 @@
+import {defineConfig} from '@junobuild/config';
+
+/** @type {import('@junobuild/config').JunoConfig} */
+export default defineConfig({
+  satellite: {
+    id: 'svftd-daaaa-aaaal-adr3a-cai',
+    source: 'build'
+  }
+});
