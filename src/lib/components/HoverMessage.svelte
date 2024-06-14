f6535734af2218c96b75995f1564534e17ec06cb
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2024-06-14 17:27:53 -0300

    Create HoverMessage.svelte
    Still unused. Needs refinement.
    
    In the meantime, there is some styling for hover messages located in the global_styles.css

---
+++ src/lib/components/HoverMessage.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let message = "This is the hover message!";
+</script>
+
+<div class="hover-container" data-message={message}>
+    <slot></slot>
+</div>
+
+<style>
+    .hover-container {
+    }
+
+    .hover-container::after {
+        content: attr(data-message);
+        visibility: hidden;
+        background-color: #f9f9f9;
+        color: #000;
+        text-align: center;
+        border-radius: 5px;
+        padding: 5px;
+        position: absolute;
+        z-index: 1;
+        opacity: 0;
+        transition: opacity 0.3s;
+        white-space: nowrap;
+        pointer-events: none;
+    }
+
+    .hover-container:hover::after {
+        visibility: visible;
+        opacity: 1;
+        top: 100%; /* Position the message below the container */
+        left: 50%;
+        transform: translateX(-50%);
+        margin-top: 10px; /* Adjust the gap between the container and the message */
+    }
+</style>
