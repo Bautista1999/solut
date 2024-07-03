51335104f7922c557799899a3192c1d4e920da18
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2024-07-03 15:51:09 -0300

    Loading dots update
    They were too big so it was improved

--- src/lib/components/MagicalDotsSmall.svelte
+++ src/lib/components/MagicalDotsSmall.svelte
@@ -52,58 +52,18 @@
 
 <div class="dots">
     {#each dotStates as dotState, index}
-        <button
-            class="dot"
-            class:move-up={dotState}
-            style="width: 20px; height: 20px;"
-        />
+        <div class="dot" class:move-up={dotState} style="" />
     {/each}
 </div>
 
 <style>
-    .loadingBlock {
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        min-height: 100vh;
-        z-index: 0;
-        margin-top: 4cm;
-    }
-
-    .loadingContent {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        background-color: white;
-        border-color: black;
-        border-width: 2px;
-        width: 30%;
-        height: 7cm;
-        box-shadow: 10px 10px 0px rgba(0, 0, 0, 1);
-        color: rgb(0, 91, 91);
-    }
-    @media (max-width: 768px) {
-        .loadingContent {
-            width: 90%; /* Adjust the width for smaller screens */
-            height: 7cm; /* Adjust the height for smaller screens */
-            box-shadow: 5px 5px 0px rgba(0, 0, 0, 1); /* Adjust the box-shadow for smaller screens */
-        }
-        .loadingBlock {
-            display: flex;
-            justify-content: center;
-            width: 100%;
-            margin-top: 5cm;
-            min-height: 100vh;
-            z-index: 0;
-        }
-    }
     .dots {
         display: flex;
         justify-content: center;
         align-items: center;
         gap: 10px;
         right: 0px;
+        z-index: 3000;
     }
     .dot {
         width: 10px;
@@ -116,9 +76,6 @@
             transform 0.3s ease,
             box-shadow 0.3s ease;
         transform: translateY(0);
-        transition:
-            transform 0.3s ease,
-            box-shadow 0.3s ease;
     }
 
     /* Define the final state (dots moved upward) */
