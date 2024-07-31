1e4e503badfa03eb6501a7678df5d5e10fda137d
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2024-07-31 17:12:03 -0300

    Update IdeaCard_container.svelte
    New cards for topics/solution screens!

--- src/lib/components/IdeaCard_container.svelte
+++ src/lib/components/IdeaCard_container.svelte
@@ -3,6 +3,7 @@
         getFeaturesOfIdea,
         getFeaturesOfSolution,
     } from "$lib/data_functions/get_functions";
+    import CardPreview from "./CardPreview.svelte";
     import IdeaCard from "./IdeaCard.svelte";
     import LoadingNew from "./LoadingNew.svelte";
     import MagicalDotsAbsoluteSmall from "./MagicalDotsAbsolut.svelte";
@@ -37,10 +38,31 @@
     </div>
 {:then data}
     {#if data.length > 0}
+        <!-- IndexDataReturn = { title: string, subtitle: string, images: string, videos: string, owner: string, type: string }; -->
         <div class="features-container">
             {#each data as feature}
                 <div class="">
-                    <IdeaCard {feature} />
+                    <!-- <IdeaCard {feature} /> -->
+                    <CardPreview
+                        idea={{
+                            key: feature.key,
+                            data: {
+                                title: feature.data.title,
+                                subtitle: feature.data.subtitle,
+                                images:
+                                    feature.data.images[0] == undefined
+                                        ? ""
+                                        : feature.data.images[0],
+                                videos:
+                                    feature.data.videos[0] == undefined
+                                        ? ""
+                                        : feature.data.videos[0],
+                                owner: feature.owner,
+                                type: type,
+                            },
+                        }}
+                        padding={10}
+                    />
                 </div>
             {/each}
         </div>
@@ -72,7 +94,7 @@
     @media (max-width: 880px) {
         .features-container {
             display: grid;
-            grid-template-columns: repeat(2, minmax(150px, 0.5fr));
+            grid-template-columns: repeat(2, minmax(100px, 0.5fr));
             grid-gap: 1rem;
             padding: 0rem; /* Add some padding around the grid if needed */
             margin: 0; /* Center the grid container */
@@ -83,7 +105,7 @@
         .features-container {
             display: grid;
             grid-template-columns: repeat(2, minmax(100px, 0.5fr));
-            grid-gap: 0.5rem;
+            grid-gap: 1rem;
             padding: 0rem; /* Add some padding around the grid if needed */
             margin: 0; /* Center the grid container */
             max-width: 600px; /* Max width of the grid to avoid very wide cards */
