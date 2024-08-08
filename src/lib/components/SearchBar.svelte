d08193f7aa633633046ec1837bf24a994b6a8352
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2024-08-08 18:33:06 -0300

    Update SearchBar.svelte

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -2,24 +2,52 @@
     export let searchText = ""; // To hold the search input text
 
     export let someFunction = () => {};
+    function clear() {
+        searchText = "";
+    }
+    export let searchComponentOpen = false;
+
+    export let closeSearchPanel = () => {};
+    export let openSearchPanel = () => {};
 </script>
 
 <div class="search-container">
+    {#if searchComponentOpen}
+        <span
+            class="material-symbols-outlined btn_clear"
+            on:click={closeSearchPanel}
+            style="margin-left:10px ;">arrow_back_ios</span
+        >
+    {/if}
     <div class="search-content">
         <input
             class="search-box"
             id="search-box"
             type="text"
-            placeholder="Search for a topic or idea..."
+            placeholder={searchComponentOpen
+                ? ""
+                : "Search for a topic or idea..."}
             bind:value={searchText}
+            on:focus={openSearchPanel}
             style="font-family: 'Barlow';"
             on:keydown={(event) => {
                 if (event.key == "Enter") {
                     someFunction();
+                    return;
+                }
+                if (event.key == "Backspace" && searchText.length == 1) {
+                    searchText = "";
+                    someFunction();
+                    return;
                 }
             }}
         />
     </div>
+    {#if searchText != ""}
+        <span class="material-symbols-outlined btn_clear" on:click={clear}
+            >close</span
+        >
+    {/if}
     <button class="search-button" on:click={someFunction}>
         <span class="material-symbols-outlined">search</span>
     </button>
@@ -36,8 +64,10 @@
             var(--tertiary-color),
             var(--forth-color)
         );
-        padding: 0.5rem 1rem;
-        border: 2px solid var(--seventh-color);
+        padding: 0.5rem 0.5rem;
+        gap: 7px;
+        border: 1px solid var(--seventh-color);
+        border-radius: 8px;
         box-shadow: 4px 4px 0px 0px var(--seventh-color);
         align-self: center;
         font-family: "Barlow";
@@ -54,6 +84,7 @@
         padding: 0.5rem;
         margin-right: 1rem;
         border: none;
+        border-radius: 8px;
         background: transparent;
         font-size: 1rem;
         width: 100%; /* Ensures it takes available space */
@@ -72,6 +103,7 @@
         justify-content: center;
         border: 1px solid var(--seventh-color);
         color: var(--tertiary-color);
+        border-radius: 8px;
     }
 
     .material-symbols-outlined {
@@ -86,10 +118,8 @@
             display: flex;
             align-items: center;
             justify-content: center;
-            width: 90%;
-            padding: 0.5rem 1rem;
-            border: 2px solid var(--seventh-color);
-            box-shadow: 4px 4px 0px 0px var(--seventh-color);
+            width: 93%;
+            max-width: 93%;
             align-self: center;
             font-family: "Barlow";
         }
