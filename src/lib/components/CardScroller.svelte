4e10b8a2fa819ee059549313e63266dcd6081002
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2024-08-08 18:33:21 -0300

    Update CardScroller.svelte

--- src/lib/components/CardScroller.svelte
+++ src/lib/components/CardScroller.svelte
@@ -1,17 +1,13 @@
 <script>
-    /**
-     * @type {string | any[]}
-     */
-    export let images = []; // Array of image URLs passed in as a prop
-    let currentImageIndex = 0;
-
     export let backFunction = () => {};
     export let forwardFunction = () => {};
+    export let disableLeftButton = true;
+    export let disableRightButton = true;
 </script>
 
 <div class="ButtonSection">
     <div class="buttons">
-        <button on:click={backFunction}>
+        <button on:click={backFunction} disabled={disableLeftButton}>
             <link
                 rel="stylesheet"
                 href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
@@ -19,7 +15,7 @@
             <span class="material-symbols-outlined"> arrow_back </span>
         </button>
         <img src="/assets/LogoSol3.png" alt="Solutio Logo" />
-        <button on:click={forwardFunction}>
+        <button on:click={forwardFunction} disabled={disableRightButton}>
             <span class="material-symbols-outlined">
                 arrow_forward
             </span></button
@@ -34,15 +30,6 @@
             "wght" 400,
             "GRAD" 0,
             "opsz" 48;
-        color: var(--tertiary-color);
-    }
-    .material-symbols-outlined:hover {
-        font-variation-settings:
-            "FILL" 0,
-            "wght" 400,
-            "GRAD" 0,
-            "opsz" 48;
-        color: var(--primary-color);
     }
 
     .ButtonSection {
@@ -55,7 +42,46 @@
         flex-direction: row; /* Align children in a row (horizontal alignment) */
         justify-content: center; /* Horizontally center the items in the container */
         align-items: center; /* Vertically align the items in the middle */
-        margin-top: 10px; /* Space between image and buttons */
+        gap: 0px;
+        background-color: rgb(
+            32,
+            32,
+            32
+        ); /* Background color of the buttons container */
+        border-radius: 8px; /* Rounded corners for the container */
+        opacity: 100%;
+        border: 1px solid var(--primary-color);
+        box-shadow: 4px 4px 0px 0px var(--primary-color);
+        transition: background-color 0.3s ease;
+        background-color: transparent;
+        border: none;
+        box-shadow: none;
+        width: fit-content;
+    }
+
+    .buttons img {
+        /* Add width and height for the image */
+        width: 29px; /* Set a fixed width */
+        height: 28px; /* Set a fixed height */
+        object-fit: contain; /* Image will maintain its aspect ratio */
+        margin: 0 5px; /* Space between buttons */
+        background-color: rgb(
+            32,
+            32,
+            32
+        ); /* Background color of the buttons container */
+        padding: 5px; /* Padding around the content inside the container */
+        border-radius: 8px; /* Rounded corners for the container */
+        opacity: 100%;
+        border: 1px solid var(--primary-color);
+        box-shadow: 4px 4px 0px 0px var(--primary-color);
+        transition: background-color 0.3s ease;
+    }
+    .buttons button {
+        margin: 0 5px; /* Space between buttons */
+        background-color: transparent;
+        cursor: pointer;
+        border: 0px solid var(--primary-color);
         background-color: rgb(
             32,
             32,
@@ -63,23 +89,66 @@
         ); /* Background color of the buttons container */
         width: fit-content; /* Container width to fit its content */
         padding: 5px; /* Padding around the content inside the container */
-        border-radius: 0px; /* Rounded corners for the container */
-        border: 0px solid var(--primary-color);
+        border-radius: 8px; /* Rounded corners for the container */
         opacity: 100%;
         border: 1px solid var(--primary-color);
+        color: var(--primary-color);
+        box-shadow: 4px 4px 0px 0px var(--primary-color);
         transition: background-color 0.3s ease;
     }
-
-    .buttons img {
-        /* Add width and height for the image */
-        width: 30px; /* Set a fixed width */
-        height: 30px; /* Set a fixed height */
-        object-fit: contain; /* Image will maintain its aspect ratio */
-    }
-    .buttons button {
+    .buttons button:hover {
         margin: 0 5px; /* Space between buttons */
         background-color: transparent;
         cursor: pointer;
         border: 0px solid var(--primary-color);
+        background-color: var(
+            --primary-color
+        ); /* Background color of the buttons container */
+        width: fit-content; /* Container width to fit its content */
+        padding: 5px; /* Padding around the content inside the container */
+        border-radius: 8px; /* Rounded corners for the container */
+        opacity: 100%;
+        border: 1px solid var(--seventh-color);
+        box-shadow: 4px 4px 0px 0px var(--seventh-color);
+        color: var(--seventh-color);
+        transition: background-color 0.3s ease;
+    }
+    .buttons button:active {
+        margin: 0 5px; /* Space between buttons */
+        background-color: transparent;
+        cursor: pointer;
+        border: 0px solid var(--primary-color);
+        background-color: var(
+            --primary-color
+        ); /* Background color of the buttons container */
+        width: fit-content; /* Container width to fit its content */
+        padding: 5px; /* Padding around the content inside the container */
+        border-radius: 8px; /* Rounded corners for the container */
+        opacity: 100%;
+        border: 1px solid var(--seventh-color);
+        box-shadow: 0px 0px 0px 0px var(--seventh-color);
+        transition:
+            background-color 0.3s ease,
+            box-shadow 0.3s ease;
+    }
+    .buttons button:disabled {
+        cursor: not-allowed;
+        margin: 0 5px; /* Space between buttons */
+        background-color: transparent;
+        border: 0px solid var(--primary-color);
+        background-color: rgb(
+            32,
+            32,
+            32
+        ); /* Background color of the buttons container */
+        width: fit-content; /* Container width to fit its content */
+        padding: 5px; /* Padding around the content inside the container */
+        border-radius: 8px; /* Rounded corners for the container */
+        opacity: 100%;
+        border: 1px solid var(--primary-color);
+        color: var(--primary-color);
+        box-shadow: 4px 4px 0px 0px var(--primary-color);
+        transition: background-color 0.3s ease;
+        opacity: 0.5;
     }
 </style>
