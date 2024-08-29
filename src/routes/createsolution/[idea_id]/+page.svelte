8029860d38bb984cdf9011471b81ea3903ea9d33
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2024-08-29 14:10:12 -0300

    Update +page.svelte

--- src/routes/createsolution/[idea_id]/+page.svelte
+++ src/routes/createsolution/[idea_id]/+page.svelte
@@ -30,6 +30,7 @@
         getUserImages,
         getUserKey,
     } from "$lib/data_functions/get_functions";
+    import WizardForm from "../../createtopic/WizardForm.svelte";
 
     /** @type {import('./$types').PageData} */
     export let data;
@@ -72,7 +73,7 @@
     let images = [];
     $: title = "";
     let subtitle = "";
-    let desc = "";
+    let description = "";
     let solutionKey = "";
     let userPicture = "";
     let tabs = ["Pledge Timeline", "Comments", "About the project"];
@@ -81,6 +82,10 @@
     let active = false;
     let subtitleActive = false;
     let newImage = "";
+    /**
+     * @type {WizardForm}
+     */
+    let wizardFormComponent;
     /**
      * @type {never[]}
      */
@@ -159,7 +164,7 @@
         } else {
             noSubtitle = false;
         }
-        if (desc == "") {
+        if (description == "") {
             noDescription = true;
         } else {
             noDescription = false;
@@ -179,7 +184,7 @@
         let solutionPost = {
             title: title,
             subtitle: subtitle,
-            description: desc,
+            description: description,
             images: images,
             videos: videos,
             categories: tags,
@@ -249,158 +254,56 @@
     let ideasTitle = [];
 </script>
 
-<div class="body">
+<div class="container">
     {#if !isLoading && !success && !error}
-        <div class="content">
-            <div class="container">
-                <div class="Subtitle">
-                    <EditSubtitle
-                        bind:title={subtitle}
-                        active={subtitleActive}
-                        bind:noSubtitle
-                    />
-                    <div style="height: 10px;"></div>
-                </div>
-                <!-- <InlineCalendar {theme} /> -->
-
-                <div class="Profile">
-                    {#await getUserImages([$UserKey])}
-                        <ProfilePicture src={""} />
-                    {:then data}
-                        <ProfilePicture src={data[0].image} userKey={user} />
-                    {/await}
-                </div>
-                <div class="Title">
-                    <br />
-                    <br />
-                    <EditTitle {active} bind:title bind:noTitle />
-                    <div style="height: 80px;"></div>
-                </div>
-
-                <div class="Breadcrumbs">
-                    <Breadcrumbs
-                        breadcrumbs={[
-                            { title: "Home", link: "" },
-                            {
-                                title: parentIdeaTitle,
-                                link: "/topic/" + parentIdeaKey,
-                            },
-                            { title: title, link: "/" },
-                        ]}
-                    />
-                    <div style="height: 10px;"></div>
-                </div>
-                <div class="Pictures">
-                    <div style="background-color: var(--secondary-color);">
-                        <ImageScrollerEdit bind:this={imageScroller} />
-                    </div>
-
-                    <br />
-                    <div
-                        style="display: flex; justify-content:left;align-items:center;
-                    gap:20px;
-                    
-                    "
+        <WizardForm
+            bind:this={wizardFormComponent}
+            bind:title
+            bind:subtitle
+            bind:description
+            bind:images
+            bind:tags
+            PostElement={onPost}
+            type={"Solution"}
+        >
+            <div slot="additional-fields">
+                <div class="form-section">
+                    <label for="ideasSelected"
+                        >Which ideas are you going to be solving?</label
                     >
-                        <input
-                            type="text"
-                            placeholder="Type the url of the image here..."
-                            class="InputText"
-                            bind:value={newImage}
-                        />
-                        <BasicButtonDarkSmall
-                            msg={"Add image"}
-                            someFunction={addImage}
-                        />
+                    <SearchBarLarger
+                        bind:ideas
+                        {parentIdeaKey}
+                        bind:ideasNames={ideasTitle}
+                        bind:noIdeas
+                    />
+                    <div class="field-info">
+                        {#if ideas.length === 0}
+                            <span class="required-field">*Required field</span>
+                        {:else}
+                            <span></span>
+                        {/if}
                     </div>
+                    <TagsDisplay
+                        bind:tags={ideasTitle}
+                        deleteFunction={deleteIdea}
+                    />
                 </div>
-
-                <div class="ActivitySection">
-                    <div class="ActivityTabs">
-                        <h3>About the project</h3>
-                    </div>
-
-                    <div class="ActivityContent">
-                        {#if activeTab === tabs[0]}
-                            <!-- <TransactionDisplay {transactions} /> -->
-                        {:else if activeTab === tabs[1]}
-                            <!-- <CommentSection project_id={key} /> -->
-                        {:else if activeTab === tabs[2]}
-                            <DescriptionEdit
-                                bind:description={desc}
-                                bind:noDescription
-                            />
+                <div class="form-section">
+                    <label for="ideasSelected"
+                        >Roadmap: set important milestones</label
+                    >
+                    <TimelineEdit bind:milestones />
+                    <div class="field-info">
+                        {#if milestones.length === 0}
+                            <span class="required-field">*Required field</span>
+                        {:else}
+                            <span></span>
                         {/if}
                     </div>
                 </div>
             </div>
-            <h3>Tags</h3>
-            <div class="tagInput">
-                <input
-                    type="text"
-                    maxlength="20"
-                    bind:value={newTag}
-                    on:keypress={(event) => {
-                        if (event.key == "Enter") {
-                            addTag();
-                        }
-                        if (event.key == ",") {
-                            addTag();
-                        }
-                        if (event.key == ".") {
-                            addTag();
-                        }
-                    }}
-                    class="InputText"
-                />
-                <BasicButtonDarkSmall msg={"Add tag"} someFunction={addTag} />
-            </div>
-            {#if tagsTooLong}
-                <p class="InputErrorMessage">
-                    You can't have more than 5 tags.
-                </p>
-            {/if}
-            <div class="tags">
-                {#each tags as tag}
-                    <!-- svelte-ignore a11y-no-static-element-interactions -->
-                    <div class="tag">
-                        <p>{tag}</p>
-                        <!-- svelte-ignore a11y-click-events-have-key-events -->
-                        <span
-                            class="material-symbols-outlined"
-                            style="cursor: pointer;"
-                            on:click={() => {
-                                deleteTag(tag);
-                            }}
-                        >
-                            delete
-                        </span>
-                    </div>
-                {/each}
-            </div>
-            <h3>Which ideas are you going to be solving ?</h3>
-            <SearchBarLarger
-                bind:ideas
-                {parentIdeaKey}
-                bind:ideasNames={ideasTitle}
-                bind:noIdeas
-            />
-            <TagsDisplay bind:tags={ideasTitle} deleteFunction={deleteIdea} />
-            <h3>Roadmap: set important milestones</h3>
-            <TimelineEdit bind:milestones />
-            <br />
-            <div
-                style="display: flex; justify-content:center;align-items:center;"
-            >
-                <BasicButtonDark
-                    msg={"Propose solution"}
-                    icon={"cognition"}
-                    someFunction={() => {
-                        onPost();
-                    }}
-                />
-            </div>
-        </div>
+        </WizardForm>
     {:else if success}
         <SuccessNew
             message={"Solution created successfully"}
@@ -427,166 +330,5 @@
 </svelte:head>
 
 <style>
-    .material-symbols-outlined {
-        font-variation-settings:
-            "FILL" 0,
-            "wght" 300,
-            "GRAD" 0,
-            "opsz" 24;
-    }
-    .material-symbols-outlined:hover {
-        font-variation-settings:
-            "FILL" 1,
-            "wght" 300,
-            "GRAD" 0,
-            "opsz" 24;
-    }
-    .tags {
-        display: flex;
-        justify-content: start;
-        align-items: center;
-        gap: 5px;
-        padding-block: 10px;
-        flex-wrap: wrap; /* Wrap tags to the next line when full */
-    }
-    .tagInput {
-        display: flex;
-        justify-content: start;
-        align-items: center;
-        gap: 20px;
-    }
-    .tag {
-        width: fit-content;
-        padding-inline: 5px;
-        padding-block: 2px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: var(--primary-color);
-        border: 1px solid var(--secondary-color);
-        color: var(--tertiary-color);
-    }
-    .body {
-        display: flex;
-        justify-content: center;
-        align-items: flex-start; /* aligns items at the top */
-        width: 100%;
-
-        min-height: 100vh;
-        z-index: 0;
-    }
-    .content {
-        width: 80%;
-        max-width: 800px;
-        text-align: left; /* aligns the text to the left */
-
-        margin: 30px auto 40px auto; /* top margin creates space from the top */
-        background-color: var(--tertiary-color);
-        padding-inline: 40px;
-        padding-block: 20px;
-        border: 1px solid var(--secondary-color);
-    }
-
-    .container {
-        display: grid;
-        grid-template-columns: 0.3fr 1.8fr 0.9fr;
-        grid-template-rows: auto;
-        gap: 4px 0px;
-        grid-auto-flow: row;
-        grid-template-areas:
-            "Breadcrumbs Breadcrumbs Breadcrumbs"
-            "Profile Title Title"
-            "Subtitle Subtitle Subtitle"
-            "Pictures Pictures Pictures"
-            "Pictures-scroller Pictures-scroller Pictures-scroller"
-            "FundingSection FundingSection FundingSection"
-            "PledgingSection PledgingSection PledgingSection"
-            "Solution-section Solution-section Solution-section"
-            "FeaturesSection FeaturesSection FeaturesSection"
-            "ActivitySection ActivitySection ActivitySection";
-    }
-
-    .Profile {
-        grid-area: Profile;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .Title {
-        grid-area: Title;
-        display: flex;
-        align-items: center; /* Aligns the content vertically */
-        padding-left: 10px; /* Optional: Adds some padding to the left */
-    }
-
-    .Subtitle {
-        grid-area: Subtitle;
-    }
-
-    .Pictures {
-        grid-area: Pictures;
-    }
-
-    .Breadcrumbs {
-        grid-area: Breadcrumbs;
-        display: flex;
-        justify-content: left;
-        text-align: left;
-        align-items: center;
-        gap: 30px;
-    }
-
-    .ActivitySection {
-        display: grid;
-        grid-template-columns: 1fr;
-        grid-template-rows: 0fr 0fr;
-        gap: 0px 0px;
-        grid-auto-flow: row;
-        grid-template-areas:
-            "ActivityTabs"
-            "ActivityContent";
-        grid-area: ActivitySection;
-    }
-
-    .ActivityTabs {
-        display: grid;
-        grid-template-columns: 1fr 0fr;
-        grid-template-rows: 0fr;
-        gap: 0px 0px;
-        grid-auto-flow: row;
-        grid-template-areas: "CommentsTab PledgersTab";
-        grid-area: ActivityTabs;
-    }
-
-    .PledgersTab {
-        grid-area: PledgersTab;
-    }
-
-    .ActivityContent {
-        grid-area: ActivityContent;
-        width: 100%;
-        height: fit-content;
-    }
-    @media (max-width: 480px) {
-        .body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-            min-height: 100vh;
-            z-index: 0;
-        }
-
-        .content {
-            width: 100%;
-            max-width: fit-content;
-            text-align: left;
-            margin: 30px auto 40px auto;
-            background-color: var(--tertiary-color);
-            padding-inline: 10px;
-            padding-block: 20px;
-            border: 1px solid var(--secondary-color);
-        }
-    }
+    @import "../../createtopic/createtopic.styles.css";
 </style>
