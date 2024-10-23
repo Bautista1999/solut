703c4edc5d9361d1d419a8553e583e113e266c93
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2024-10-23 16:21:25 -0300

    Create ModalConfirmation.svelte

---
+++ src/lib/components/ModalConfirmation.svelte
@@ -0,0 +1,88 @@
+<script>
+    import { confirmationModal } from "$lib/stores/other_stores";
+    import BasicButtonDarkSmall from "./BasicButton_Dark_Small.svelte";
+    import ErrorModalNew from "./ErrorModalNew.svelte";
+    import LoadingModalNew from "./LoadingModalNew.svelte";
+    import MagicalDots from "./magicalDots.svelte";
+    import MagicalDotsAbsolut from "./MagicalDotsAbsolut.svelte";
+    import MagicalDotsSmall from "./MagicalDotsSmall.svelte";
+    import Modal from "./modal.svelte";
+    import Success from "./success.svelte";
+    import SuccessModalNew from "./SuccessModalNew.svelte";
+
+    export let message = "";
+    export let someFunction = async () => {};
+
+    export let loading = false;
+    export let success = false;
+
+    export let error = false;
+    export let errorMsg = "";
+    export let successMsg = "";
+    export let loadingMsg = "";
+</script>
+
+<Modal
+    bind:isOpen={$confirmationModal}
+    close={() => {
+        confirmationModal.set(false);
+    }}
+    style={"width:350px;"}
+>
+    <div class="deleteModal">
+        {#if loading}
+            <LoadingModalNew message={loadingMsg} />
+        {:else if success}
+            <SuccessModalNew message={successMsg} />
+        {:else if error}
+            <ErrorModalNew
+                error={errorMsg}
+                someFunction={() => {
+                    error = false;
+                    confirmationModal.set(false);
+                }}
+            />
+        {:else}
+            <div class="elements">
+                <p style="font-size: larger; ">{message}</p>
+                <div class="VerticallyAligned">
+                    <BasicButtonDarkSmall
+                        msg="Yes"
+                        someFunction={async () => {
+                            await someFunction();
+                        }}
+                    />
+                    <BasicButtonDarkSmall
+                        msg="No"
+                        someFunction={() => {
+                            confirmationModal.set(false);
+                        }}
+                    />
+                </div>
+            </div>
+        {/if}
+    </div>
+</Modal>
+
+<style>
+    .deleteModal {
+        margin: 5px;
+        margin-top: 50px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-size: large;
+        gap: 30px;
+        height: 100%;
+    }
+    .elements {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-size: large;
+        gap: 30px;
+        height: 100%;
+    }
+</style>
