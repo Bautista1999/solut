0515a39287ba7bde6c90b9bd0286b0ab477f0b1e
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2024-10-23 16:24:00 -0300

    Update metadata_functions.js

--- src/lib/SEO and metadata/metadata_functions.js
+++ src/lib/SEO and metadata/metadata_functions.js
@@ -250,33 +250,7 @@ export async function updateSiteMapxml(id) {
   console.log("Sitemap: ",sitemap)
   // If the sitemap doesn't exist, start a new one
   if (!sitemap) {
-    sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-<url>
-  <loc>https://solutio.one</loc>
-  <lastmod>2024-09-18T19:39:50+00:00</lastmod>
-  <priority>1</priority>
-</url>
-  <url>
-    <loc>https://solutio.one/createtopic</loc>
-    <lastmod>2024-09-18T19:39:50+00:00</lastmod>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://solutio.one/feed</loc>
-    <lastmod>2024-09-18T19:39:50+00:00</lastmod>
-    <priority>0.8</priority>
-  </url>
-  <url>
-    <loc>https://solutio.one/myideas</loc>
-    <lastmod>2024-09-18T19:39:50+00:00</lastmod>
-    <priority>0.8</priority>
-  </url>
-</urlset>`;
+    return;
   }
 
   // Create the new topic URL
