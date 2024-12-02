342ab9664181cda09dd4fb2438c773790523241f
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2024-12-02 19:46:27 -0300

    Update lib.rs

--- src/satellite/src/lib.rs
+++ src/satellite/src/lib.rs
@@ -18,8 +18,10 @@ mod notifications;
 mod pledges;
 mod reputation;
 mod scheduled;
+mod topics_service;
 mod types;
 mod user_information;
+use crate::types::interface::IndexResponse;
 use base64::encode; // make sure to add `base64` to dependencies in Cargo.toml
 use ic_cdk::api::{self, set_global_timer, time};
 use ic_cdk_macros::{query, update};
