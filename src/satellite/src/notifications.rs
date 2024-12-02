a6a5cd33cb63281c19ed8f2db0c91def71f869d1
Bautista1999 <106879776+Bautista1999@users.noreply.github.com>
2024-12-02 12:42:50 -0300

    Update notifications.rs
    Notifications creation (WIP)

--- src/satellite/src/notifications.rs
+++ src/satellite/src/notifications.rs
@@ -1,6 +1,6 @@
 use crate::quickqueries::get_doc_owner;
 use crate::reputation::get_user_reputation;
-use crate::types::interface::{IndexSearch, PledgeData, PledgeUser, TotalPledging};
+use crate::types::interface::{IndexSearch, Notification, PledgeData, PledgeUser, TotalPledging};
 use crate::{get_document_description_or_default, get_document_version_or_default};
 use base64::encode; // make sure to add `base64` to dependencies in Cargo.toml
 use bytes::Bytes;
@@ -25,11 +25,32 @@ use std::convert::TryFrom;
 use std::iter::Filter;
 use uuid::Uuid;
 
-//TODO: Figure out how to create a random key.
-pub fn send_single_notification(target_user: String, receiver: String) -> Result<(), String> {
+#[update]
+pub fn send_single_notification(
+    sender: String,
+    receiver: String,
+    notification: Notification,
+) -> Result<(), String> {
     // encode_doc_data(data)
     let id = Uuid::new_v4();
     let key = id.to_string();
+    let encoded_data = match encode_doc_data(&notification) {
+        Ok(vec) => vec,
+        Err(err) => return Err(format!("Failed to encode notification: {}", err)),
+    };
 
+    // Specify the collection key
+    let collection_key = "notification".to_string(); // Adjust collection name as needed
+    let controller = Principal::from_text("rfamr-niaaa-aaaam-acmta-cai").unwrap();
+    // Create a document for storage
+    let doc = SetDoc {
+        data: encoded_data,
+        version: Some(1),                    // Optional versioning
+        description: Some(receiver.clone()), // Optional description
+    };
+    match set_doc_store(controller, collection_key, key, doc) {
+        Ok(_) => {}
+        Err(err) => return Err(format!("Failed to store notification: {}", err)),
+    };
     Ok(())
 }
